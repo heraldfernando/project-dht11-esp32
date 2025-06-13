d3fdcf3a867777a43e47d09c99db33b63a8d21b0
heraldfernando <108991020+heraldfernando@users.noreply.github.com>
2025-06-13 14:26:19 +0700

    Add files via upload

---
+++ project-dht11-esp32.ino
@@ -0,0 +1,29 @@
+#include "DHT.h"
+
+#define DHTPIN 4        // Pin data DHT terhubung ke GPIO 4
+#define DHTTYPE DHT11   // Tipe sensor DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+  float suhu = dht.readTemperature();     // dalam °C
+  float kelembaban = dht.readHumidity();  // dalam %
+
+  if (isnan(suhu) || isnan(kelembaban)) {
+    Serial.println("Gagal membaca dari sensor DHT!");
+  } else {
+    Serial.print("Suhu: ");
+    Serial.print(suhu);
+    Serial.print("°C  |  Kelembaban: ");
+    Serial.print(kelembaban);
+    Serial.println("%");
+  }
+
+  delay(2000);  // baca setiap 2 detik
+}
+
